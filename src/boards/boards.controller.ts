8bb3772bb4b1bfacb0942f2cffb107036a980641
Jay <myeong8730@gmail.com>
2023-07-08 16:03:34 +0900

    [Board] id값으로 특정 게시글 조회 기능 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { BoardsService } from "./boards.service";
 import { Board } from "./board.model";
 import { CreateBoardDto } from "./dto/create-board.dto";
@@ -12,10 +12,17 @@ export class BoardsController {
     return this.boardsService.getAllBoards();
   }
 
+  // 게시글 작성
   @Post()
   createBoard(
     @Body() createBoardDto: CreateBoardDto
   ): Board {
     return this.boardsService.createBoard(createBoardDto);
+  }
+
+  // id값으로 특정 게시글 조회
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
   }
 }
