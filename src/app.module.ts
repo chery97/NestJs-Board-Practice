c59f240b3d73c34ad7ef72f33140d4b30db8995a
chery97 <myeong8730@gmail.com>
2023-07-08 15:47:03 +0900

    [Board] 컨트롤러, 서비스, 모델 추가 및 게시글 작성 로직 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsService } from './boards/boards.service';
+import { BoardsController } from './boards/boards.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BoardsController],
+  providers: [AppService, BoardsService],
 })
 export class AppModule {}
